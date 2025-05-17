c91e3dbc937c42e3c0a9c3737f5ac35c83f1d569
carson <75181446+crsnao@users.noreply.github.com>
2025-05-17 11:32:28 -0300

    Update and rename nicoly/scripts/analise_idh.R to nicoly/preprocessamento_manaus_idh.R

--- nicoly/preprocessamento_manaus_idh.R
+++ nicoly/preprocessamento_manaus_idh.R
@@ -8,7 +8,6 @@ library(tidyr)
 library(readr)
 library(readxl)
 library(sf)
-library(corrplot)
 
 # Lê um arquivo GeoPackage contendo dados espaciais da cidade de Manaus
 manaus = st_read("/home/carson/R/nicoly_ma_utc/manaus_nicoly.gpkg")
@@ -30,20 +29,3 @@ df_regioes_D <- df %>%
 
 # Tenta salvar `df_regioes`, mas o objeto correto é `df_regioes_D`
 write_sf(df_regioes, "/home/carson/R/nicoly_ma_utc/manaus_regiões.gpkg")
-
-# Define variáveis para análise de correlação
-x = df_regioes_D$IDHM
-y = df_regioes_D$X_normpland
-
-# Gera um gráfico de dispersão com linha de regressão
-plot(x, y,
-     xlab = "Cobertura Arbórea (%)",
-     ylab = "Temperatura Média (°C)",
-     main = "Correlação entre Arborização e Temperatura",
-     pch = 19, col = "blue")
-
-abline(lm(IDHM ~ landsat_mean, data = df), col = "red", lwd = 2)  # <-- possível inconsistência com os dados usados
-
-# Calcula correlações entre as variáveis (Pearson e Spearman)
-cor(x, y, method = "pearson")
-cor(x, y, method = "spearman")
